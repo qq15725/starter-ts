a2dec3876e431baa66a3fd546fd0d7a4cae3819e
wengxiangmin <157215725@qq.com>
2024-09-24 15:57:56 +0800

    feat: update

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,15 @@
 import { basename, resolve } from 'path'
 import { defineConfig } from 'vite'
-import { browser, exports, module, name } from './package.json'
+import { browser, name } from './package.json'
 
 const resolvePath = (str: string) => resolve(__dirname, str)
 
 export default defineConfig({
   build: {
     lib: {
-      formats: ['es', 'cjs', 'umd'],
+      formats: ['umd'],
       fileName: format => {
-        if (format === 'es') return basename(module)
         if (format === 'umd') return basename(browser)
-        if (format === 'cjs') return basename(exports['.'].require)
         return `${ name }.${ format }`
       },
       entry: resolvePath('./src/index.ts'),
