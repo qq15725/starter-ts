b5ffc39dd321733dd34e732076e4d59d5b253818
wengxiangmin <157215725@qq.com>
2024-09-24 16:13:51 +0800

    feat: ci

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { basename, resolve } from 'path'
+import { basename, resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import { browser, name } from './package.json'
 
@@ -8,12 +8,13 @@ export default defineConfig({
   build: {
     lib: {
       formats: ['umd'],
-      fileName: format => {
-        if (format === 'umd') return basename(browser)
-        return `${ name }.${ format }`
+      fileName: (format) => {
+        if (format === 'umd')
+          return basename(browser)
+        return `${name}.${format}`
       },
       entry: resolvePath('./src/index.ts'),
-      name: name.replace(/-(\w)/ig, (_, v) => v.toUpperCase()),
+      name: name.replace(/-(\w)/g, (_, v) => v.toUpperCase()),
     },
   },
 })
